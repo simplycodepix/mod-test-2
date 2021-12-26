64aa0109b4ad9a6e283edef3f86ea1ecf4e1c7a3
Vladislav Godunov <simplycodepix@gmail.com>
2021-12-26 15:57:00 +0200

    Added test for formatTicket

--- src/test/java/shoppingCart/ShoppingCartTest.java
+++ src/test/java/shoppingCart/ShoppingCartTest.java
@@ -17,6 +17,26 @@ class ShoppingCartTest {
     }
 
     @Test
+    void formatTicketCheck() {
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItem("Apple", 0.99, 5, ShoppingCart.ItemType.NEW);
+        cart.addItem("Banana", 20.00, 4, ShoppingCart.ItemType.SECOND_FREE);
+        cart.addItem("A long piece of toilet paper", 17.20, 1, ShoppingCart.ItemType.SALE);
+        cart.addItem("Nails", 2.00, 500, ShoppingCart.ItemType.REGULAR);
+
+        String expected = "# Item                          Price Quan. Discount   Total \n"
+                + "------------------------------------------------------------\n"
+                + "1 Apple                          $.99     5        -   $4.95 \n"
+                + "2 Banana                       $20.00     4      50%  $40.00 \n"
+                + "3 A long piece of toilet paper $17.20     1      70%   $5.16 \n"
+                + "4 Nails                         $2.00   500      50% $500.00 \n"
+                + "------------------------------------------------------------\n"
+                + "4                                                    $550.11 ";
+
+        assertEquals(expected, cart.formatTicket());
+    }
+
+    @Test
     void appendFormattedCheck() {
         StringBuilder sb = new StringBuilder();
         ShoppingCart.appendFormatted(sb, "Append Formatted Check", 0, 14);
