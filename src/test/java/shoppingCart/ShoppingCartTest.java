de775c892e266b32ba63b1189d00b6a802826781
Vladislav Godunov <simplycodepix@gmail.com>
2021-12-26 14:46:15 +0200

    initial commit

---
+++ src/test/java/shoppingCart/ShoppingCartTest.java
@@ -0,0 +1,26 @@
+package shoppingCart;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void appendFormatted() {
+    }
+
+    @Test
+    void calculateDiscount() {
+    }
+}
